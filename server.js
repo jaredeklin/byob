4c27d31f011159ff1f68f18d1a6bc9c545c60698
Michael Gatewood <michael@mngatewood.com>
2018-05-15 13:26:29 -0600

    Adds GET for artists and albums

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+app.set('port', process.env.PORT || 3000);
+
+app.locals.title = 'BYOB';
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/api/v1/artists', (request, response) => {
+  database('artists').select()
+    .then(artists => response.status(200).json(artists))
+    .catch(error => response.status(404).json({error}))
+})
+
+app.get('/api/v1/albums', (request, response) => {
+  database('albums').select()
+    .then(albums => response.status(200).json(albums))
+    .catch(error => response.status(404).json({error}))
+})
 
 
 
@@ -15,6 +33,8 @@ const database = require('knex')(configuration);
 
 
 
-
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} listening on localhost:${app.get('port')}.`)
+})
 
 module.exports = { app, database }
