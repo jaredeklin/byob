b6e18d41ac30a4d63bfe9addb7cbc0d76dfa3fe2
Michael Gatewood <michael@mngatewood.com>
2018-05-14 18:14:03 -0600

    Adds albums to top 30 artists

--- public/js/index.js
+++ public/js/index.js
@@ -6,10 +6,9 @@ const fs = require('fs')
 
 let getTopArtists = async () => {
   try {
-    const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${apiKey}&format=json`)
+    const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&limit=30&api_key=${apiKey}&format=json`)
     const data = await response.json();
     const cleanData = await cleanArtist(data.artists.artist)
-    console.log(cleanData)
     const artistData = JSON.stringify(cleanData, null, 2)
     await fs.writeFile('./artist-data.json', artistData, 'utf8', error => {
       if(error) {
@@ -17,7 +16,6 @@ let getTopArtists = async () => {
       }
     })
     console.log('data saved to file')
-    // console.log(artistIds)
 
   } catch (error) {
     console.log(error)
@@ -29,15 +27,14 @@ getTopArtists()
 const cleanArtist = (data) => {
   
   const promises = data.map( async (artist) => {
-    // let albums = await getAlbums(artist.name)
-    // console.log(albums)
+    let albums = await getAlbums(artist.name)
 
     const artistObj = {
       name: artist.name,
       mbid: artist.mbid,
       url: artist.url,
-      image: artist.image[4]['#text']
-      // albums: albums
+      image: artist.image[4]['#text'],
+      albums: albums
     }
 
     return artistObj
@@ -49,7 +46,6 @@ const cleanArtist = (data) => {
 const getAlbums = async (artist) => {
   const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist}&limit=5&api_key=${apiKey}&format=json`)
   const albumData = await response.json(); 
-  // console.log(albumData.topalbums)
   const cleanAlbumData =  albumData.topalbums.album.map(album => ({
     name: album.name,
     url: album.url,
@@ -58,18 +54,3 @@ const getAlbums = async (artist) => {
 
   return Promise.all(cleanAlbumData)
 }
-
-
-
-// let topArtists = getTopArtists();
-// console.log(topArtists) 
-
-//.then(result => {
-    // let condition = JSON.stringify({ condition: result })
-
-    // fs.writeFile('./weather-data.json', condition, 'utf8', error => {
-    //   if(error) {
-    //     return console.log(error)
-    //   }
-    // })
-    // console.log('data saved to file')
